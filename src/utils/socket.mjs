7e67ad5293407b8f92c6a5a5273ff67857f15a23
123Nishiwei <shiwei.ni>
2022-08-01 17:33:14 +0800

    socket

--- src/utils/socket.mjs
+++ src/utils/socket.mjs
@@ -1,24 +1,31 @@
-import SockJS from 'sockjs-client' // socket
+//import SockJS from 'sockjs-client' // socket event.js:8 Uncaught ReferenceError: global is not defined at node_modules/so
+import SockJS from 'sockjs-client/dist/sockjs.js' // socket
 import Stomp from 'stompjs'
 
-class mySocket {
-  static instance = null
-  constructor() {
-    this.wsUrl = '127.0.0.1:7001' // websocket url地址
-    console.log('new mySocket()',new mySocket());
-    mySocket.instance = new mySocket()    
-    this.socketClient = null
-    mySocket.instance.connect()
+class MySocket {
+  constructor(wsUrl = 'http://127.0.0.1:7001/') {
+    this.wsUrl = 'http://127.0.0.1:7001/socket.io/?usekey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIxMjMiLCJpYXQiOjE2NTkwODEzODksImV4cCI6MTY1OTA4NDk4OX0.dTdEC5eTBeaFbweDFTEpY14c_S8oi-P4OsQVeN03OTM&EIO=3&transport=websocket&sid=kL1bWLGZM1RpJAsMAAAD' // websocket url地址
+    this.stompClient = null
+    this.connect()
   }
   // 建立websocket连接
   connect() {
     const host = this.wsUrl
-    const token = localStorage.getItem('m-token')
-    const url = `${host}?${token}`
+    // const token = localStorage.getItem('m-token')
+    const token = ''
+    // const url = `${host}?${token}`
+    const url = `${host}`
+
     const socket = new SockJS(url)
-    this.socketClient = Stomp.over(socket)
-    this.socketClient.connect('connect',
+    // 获取STOMP子协议的客户端对象
+    this.stompClient = Stomp.over(socket)
+    this.stompClient.connect('connect',
       (success) => {
+        stompClient.subscribe('/server', function (message) {
+          console.warn(message.body);
+          console.warn(JSON.parse(message.body));
+
+        });
         console.log('webSocket连接成功:', success)
       },
       (error) => {
@@ -28,14 +35,15 @@ class mySocket {
   }
   // 销毁ws连接
   disconnect () {
-    this.socketClient?.disconnect()
+    this.stompClient?.disconnect()
   }
 }
+const mySocket = new MySocket()
+export default mySocket
 
-export default mySocket.instance
 
 //test
-// mySocket.instance.subscribe(`/`, (message) => {
+// MySocket.instance.subscribe(`/`, (message) => {
 //     const res = JSON.parse(message.body)
 //     console.log(res);
 // })
--- src/views/MyAbout.vue
+++ src/views/MyAbout.vue
@@ -4,6 +4,26 @@
   </MyCard>
 </template>
 <script setup>
-import { ref } from "vue";
+// import mySocket from '@/utils/socket.mjs'
+import { ref, onMounted } from "vue";
 import MyCard from "@/components/MyCard.vue";
+import mySocket from "@/utils/socket.mjs"
+onMounted(() => {
+  // const ws = new WebSocket('ws://127.0.0.1:7001/socket.io/?usekey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIxMjMiLCJpYXQiOjE2NTkwODEzODksImV4cCI6MTY1OTA4NDk4OX0.dTdEC5eTBeaFbweDFTEpY14c_S8oi-P4OsQVeN03OTM&EIO=3&transport=websocket&sid=kL1bWLGZM1RpJAsMAAAD')
+  // ws.onopen = function (evt) {
+  //   console.log("Connection open ...");
+  //   ws.send("Hello WebSockets!");
+  // };
+
+  // ws.onmessage = function (evt) {
+  //   console.log("Received Message: " + evt.data);
+  //   ws.close();
+  // };
+
+  // ws.onclose = function (evt) {
+  //   console.log("Connection closed.");
+  // };
+
+})
+
 </script>
